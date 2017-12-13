8fc69e7aa537ba77f45ab47df4ba0d3942e37b75
Anthony Broome <anthonybroome87@gmail.com>
2017-12-13 12:55:04 +0000

    live changes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -6,8 +6,8 @@ variable "azurerm_tenant_id" {}
 variable "location" { default = "UK West"}
 variable "tag-env" { default = "dev"}
 
-variable "vnet-range" { default = "10.0.0.0/20"}
-variable "app-subnet-range" { default = "10.0.0.0/24"}
+variable "vnet-range" { default = "11.0.0.0/20"}
+variable "app-subnet-range" { default = "11.0.0.0/24"}
 
 
 variable "vm1-username" { default = "demo-admin"}
--- tests/demo-profile/controls/demo-rg.rb
+++ tests/demo-profile/controls/demo-rg.rb
@@ -4,14 +4,13 @@ control 'Subscription Tests' do
   title 'Demo Resource Groups'
 
   describe azure_generic_resource(group_name: 'demo-rg') do
-     its('total') { should eq 0 }
-     #its('Microsoft.Compute/virtualMachines') { should eq 1 }
-     #its('Microsoft.Network/networkInterfaces') { should eq 1 }
-     #its('Microsoft.Network/publicIPAddresses') { should eq 1 }
-     #its('Microsoft.Network/networkSecurityGroups') { should eq 1 }
-     #its('Microsoft.Storage/storageAccounts') { should eq 0 }
-     #its('Microsoft.Network/virtualNetworks') { should eq 1 }
-     #its('Microsoft.Compute/disks') { should eq 0 }
+     its('total') { should eq 6 }
+     its('Microsoft.Compute/virtualMachines') { should eq 1 }
+     its('Microsoft.Network/networkInterfaces') { should eq 1 }
+     its('Microsoft.Network/publicIPAddresses') { should eq 1 }
+     its('Microsoft.Network/networkSecurityGroups') { should eq 1 }
+     its('Microsoft.Network/virtualNetworks') { should eq 1 }
+
   end
 
   # describe azure_generic_resource(group_name: 'demo-rg', name: 'demo-vnet') do
@@ -19,9 +18,9 @@ control 'Subscription Tests' do
   #   its('properties.addressSpace.addressPrefixes') { should include '10.0.0.0/20'}
   # end
 
-  # describe azure_generic_resource(group_name: 'demo-rg', name: 'demo-vnet') do
-  #   its('location') { should cmp 'ukwest' }
-  #   its('properties.addressSpace.addressPrefixes') { should include '10.0.0.0/20'}
-  # end
+  describe azure_generic_resource(group_name: 'demo-rg', name: 'demo-vnet') do
+    its('location') { should cmp 'ukwest' }
+    its('properties.addressSpace.addressPrefixes') { should include '10.0.0.0/20'}
+  end
 
 end
