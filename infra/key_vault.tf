dcd23cd3c352a9aed5457fde6e636b286c80fc9d
Anthony Broome <anthonybroome87@gmail.com>
2018-04-22 18:26:15 +0100

    Adding random uid to vault due to azure needing global uniquenessgit add .git add .git add .

--- infra/key_vault.tf
+++ infra/key_vault.tf
@@ -1,7 +1,12 @@
 data "azurerm_client_config" "current" {}
 
+resource "random_string" "vault_uid" {
+  length = 16
+  special = false
+}
+
 resource "azurerm_key_vault" "demo-vault" {
-  name = "demo-vault"
+  name = "vault${random_string.vault_uid.result}"
   location = "${var.location}"
   resource_group_name = "${azurerm_resource_group.demo-rg.name}"
   tenant_id = "${data.azurerm_client_config.current.tenant_id}"
